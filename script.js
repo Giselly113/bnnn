57d1ff812826a3fb6f2ef3b4dea67c4b1e52066b
Promomastt <outlettanamaooficial@gmail.com>
2024-11-12 10:09:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Exemplo de funcionalidade futura para salvar promoções favoritas
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Código para manipulações futuras
+});
